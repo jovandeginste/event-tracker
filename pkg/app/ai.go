e6c8baef72f92720a253799b774bf552133a2ee0
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2024-07-20 16:20:53 +0200

    Improve prompt by including all previous events
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- pkg/app/ai.go
+++ pkg/app/ai.go
@@ -16,12 +16,7 @@ func (a *App) AddAITags(e *Event) error {
 		return nil
 	}
 
-	c, err := a.AllCategories()
-	if err != nil {
-		return err
-	}
-
-	at, err := e.AutoTags(c)
+	at, err := a.AutoTags(e)
 	if err != nil {
 		return err
 	}
@@ -31,28 +26,47 @@ func (a *App) AddAITags(e *Event) error {
 	return a.UpdateEvent(e)
 }
 
-func (e *Event) AutoTags(cats []string) ([]string, error) {
+func (a *App) AutoTags(e *Event) ([]string, error) {
+	log.Print("Generating categories for: ", e.Summary)
+
 	client, err := api.ClientFromEnvironment()
 	if err != nil {
 		return nil, err
 	}
 
+	evs, err := a.AllEvents()
+	if err != nil {
+		return nil, err
+	}
+
+	div := "####"
 	noStream := false
 	c := fmt.Sprintf(
-		"Summary: %s\nDescription: %s\nLocation: %s\nTags: %s",
+		div+"\nNew event:\nSummary: %s\nDescription: %s\nLocation: %s\nTags: %s\n"+div,
 		e.Summary, e.Description(),
 		e.Location(), strings.Join(e.Categories, ", "),
 	)
 
+	p := `You will be provided with details for all existing events, and one new event, all divided with ` + div + `. The event details will be formatted as "key: value". Classify the new event with any tags from the previous events, if any are relevant.
+Suggest additional new tags if no existing tags are relevant.
+Use the same language as the event summary and description.
+Provide only json output with the following keys: tags and new_tags. Don't add any comments or other text to the output.
+
+Existing events:
+`
+
+	for _, ev := range evs {
+		p += fmt.Sprintf(
+			div+"\nSummary: %s\nDescription: %s\nLocation: %s\nTags: %s\n",
+			ev.Summary, ev.Description(),
+			ev.Location(), strings.Join(ev.Categories, ", "),
+		)
+	}
+
 	// By default, GenerateRequest is streaming.
 	req := &api.GenerateRequest{
-		Model: "mistral",
-		System: `You will be provided with details for an event. The event details will be formatted as "key: value". Classify the event with any of the existing tags, if any are relevant.
-Suggest additional new tags if no existing tags are relevant.
-Provide only json output with the following keys: tags and new_tags. Don't add any comments or other text to the json.
-
-Existing tags:
-` + strings.Join(cats, "\n"),
+		Model:  "mistral",
+		System: p,
 		Prompt: c,
 		Stream: &noStream,
 	}
@@ -63,7 +77,6 @@ Existing tags:
 	}
 
 	respFunc := func(resp api.GenerateResponse) error {
-		log.Print("Summary: ", e.Summary)
 		log.Print("Response: ", resp.Response)
 
 		return json.Unmarshal([]byte(resp.Response), &res)
